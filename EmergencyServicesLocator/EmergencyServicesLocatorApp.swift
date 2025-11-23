32cb1850aca750759f9ab9de20da1d3f16b68818
JMayna <j.maynard.20122@gmail.com>
2025-11-23 16:26:44 -0500

    Initial Commit

---
+++ EmergencyServicesLocator/EmergencyServicesLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmergencyServicesLocatorApp.swift
+//  EmergencyServicesLocator
+//
+//  Created by Jordan Maynard on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct EmergencyServicesLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmergencyServicesLocatorTests/EmergencyServicesLocatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmergencyServicesLocatorTests.swift
+//  EmergencyServicesLocatorTests
+//
+//  Created by Jordan Maynard on 11/23/25.
+//
+
+import Testing
+@testable import EmergencyServicesLocator
+
+struct EmergencyServicesLocatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
